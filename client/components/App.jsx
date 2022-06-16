77c2d6ce6bbfc53944301e74f107e7c37ecfcbe6
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-17 08:43:32 +1200

    added functionality for sound

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import useSound from 'use-sound'
+
+import tune from '../sounds/but-tweet.mp3'
+
 const App = () => {
   const [count, setCount] = useState(0)
+  // let ownTune = new Audio(tune)
+
+  const [play] = useSound(tune)
 
   return (
     <div>
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Click me!</button>
+      <button onClick={play}>Play the tune</button>
       <Routes>
         <Route path='/test' element={<h1>TEST ROUTE</h1>} />
       </Routes>
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.mp3$/,
+        loader: 'file-loader'
+      }
     ],
   },
   resolve: {
